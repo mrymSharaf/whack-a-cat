3824ef4b0a19d8ad343c4dec44ce2b811cd8171e
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-12 00:12:06 +0300

    add styling and createGrid function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,31 @@
-function init(){
+function init() {
+
+    /*-------------------------------- Constants --------------------------------*/
+
+
+
+    /*---------------------------- Variables (state) ----------------------------*/
+    let cells = 6
+
+
+    /*------------------------ Cached Element References ------------------------*/
+    const gridElm = document.querySelector('.grid')
+
+
+    /*-------------------------------- Functions --------------------------------*/
+    function createGrid(){
+        for(let i = 0; i < cells; i++ ){
+            const cell = document.createElement('div')
+            cell.classList.add('cell')
+            gridElm.appendChild(cell)
+        }
+    }
+    createGrid()
+
+
+    /*----------------------------- Event Listeners -----------------------------*/
+
+
 
 }
 document.addEventListener('DOMContentLoaded', init)
