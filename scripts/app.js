72ef846b99e24ebc4582cae18f83e5650e406f60
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-12 11:03:00 +0300

    add cat to cell

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,43 @@
 function init() {
 
     /*-------------------------------- Constants --------------------------------*/
+    const totalCells = 6
 
 
 
     /*---------------------------- Variables (state) ----------------------------*/
-    let cells = 6
-
+    let cells = []
 
     /*------------------------ Cached Element References ------------------------*/
     const gridElm = document.querySelector('.grid')
 
 
     /*-------------------------------- Functions --------------------------------*/
-    function createGrid(){
-        for(let i = 0; i < cells; i++ ){
+    function createGrid() {
+        for (let i = 0; i < totalCells; i++) {
             const cell = document.createElement('div')
             cell.classList.add('cell')
             gridElm.appendChild(cell)
+            cells.push(cell)
         }
     }
     createGrid()
 
+    function addCat() {
+        cells.forEach(cell => {
+            cell.classList.remove('cat')
+            cell.textContent = ''
+        })
+
+        const randomIndx = Math.floor(Math.random() * cells.length)
+        const randomCell = cells[randomIndx]
+        randomCell.classList.add('cat')
+    }
+
+    function render() {
+        addCat()
+    }
+    render()
 
     /*----------------------------- Event Listeners -----------------------------*/
 
