9c85580249110732d240bf1036e9e81a28a7ed90
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-12 14:31:23 +0300

    add levels functionality

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,22 @@ function init() {
 
     /*-------------------------------- Constants --------------------------------*/
     const totalCells = 6
-
+    const time = 30
 
 
     /*---------------------------- Variables (state) ----------------------------*/
     let cells = []
     let score = 0
     let speed
+    let levelSpeed = 3000
 
     /*------------------------ Cached Element References ------------------------*/
     const gridElm = document.querySelector('.grid')
     const scoreElm = document.querySelector('#score')
     const playbtn = document.querySelector('#play-game')
+    const easyLvl = document.querySelector('#easy')
+    const midLvl = document.querySelector('#medium')
+    const hardLvl = document.querySelector('#hard')
 
     /*-------------------------------- Functions --------------------------------*/
     function createGrid() {
@@ -25,39 +29,57 @@ function init() {
             cells.push(cell)
         }
     }
-    
+
     function addCat() {
         cells.forEach(cell => {
             cell.classList.remove('cat')
             cell.textContent = ''
         })
-        
+
         const randomIndx = Math.floor(Math.random() * cells.length)
         const randomCell = cells[randomIndx]
         randomCell.classList.add('cat')
     }
-    
+
     function clickCat(event) {
         if (event.target.classList.contains('cat')) {
             event.target.classList.remove('cat')
             addCat()
             score += 5
-            scoreElm.textContent = (score)
+            scoreElm.textContent = score
         }
     }
-    
+
     function startGame() {
         addCat()
-        speed = setInterval(addCat, 2000)
+        speed = setInterval(addCat, levelSpeed)
     }
-    
+
+    function levelChange() {
+        clearInterval(speed)
+        addCat()
+        speed = setInterval(addCat, levelSpeed)
+    }
+
     function render() {
         createGrid()
     }
-    render()
 
     /*----------------------------- Event Listeners -----------------------------*/
     playbtn.addEventListener('click', startGame)
+    easyLvl.addEventListener('click', () => {
+        levelSpeed = 3000
+        levelChange()
+    })
+    midLvl.addEventListener('click', () => {
+        levelSpeed = 1000
+        levelChange()
+    })
+    hardLvl.addEventListener('click', () => {
+        levelSpeed = 500
+        levelChange()
+    })
 
+    render()
 }
 document.addEventListener('DOMContentLoaded', init)
